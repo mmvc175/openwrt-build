777821c0ae4442b3ded65b80e935c263c1388c11
mmvc175 <55680229+mmvc175@users.noreply.github.com>
2023-09-21 16:17:43 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,6 +12,7 @@
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/192.168.1.1/192.168.1.6/g' openwrt/package/base-files/files/bin/config_generate
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
